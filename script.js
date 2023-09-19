827546a234a4a6506d99b415f7f27475cd377e65
Darlan Martins <122477884+Darlan0307@users.noreply.github.com>
2023-09-19 13:47:51 -0300

    animação da navegação primária e ajustes nos cards

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ escrevendoLetras();
 
 const ativaMenu = document.querySelector('.fa-bars');
 
+const navMenu = document.querySelector('header .navegacao-primaria');
+
 ativaMenu.addEventListener('click',()=>{
     ativaMenu.classList.toggle('fa-x');
+    navMenu.classList.toggle('ativado');
 })
